2362352cf347300eaab5c60c1f11d36fe20135b0
SUN <siksik2782259@gmail.com>
2024-03-08 16:32:53 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/Appconfig.java
+++ src/main/java/hello/core/Appconfig.java
@@ -3,6 +3,7 @@ package hello.core;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -13,18 +14,27 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Appconfig {
+    // call Appconfig.memberSerivce
+    // call Appconfig.memberRepository
+    // call Appconfig.memberRepository
+    // call Appconfig.orderService
+    // call Appconfig.memberRepository
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call Appconfig.memberSerivce");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public static MemoryMemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
+        System.out.println("call Appconfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call Appconfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,28 @@
+package hello.core.singleton;
+
+import hello.core.Appconfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(Appconfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        System.out.println("memberService -> MemberRepository =  " + memberService.getMemberRepository());
+        System.out.println("orderService -> MemberRepository  = " + orderService.getMemberRepository());
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
