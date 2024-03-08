000f7ff57553821f54b8e28408560c533ba45ae1
SUN <siksik2782259@gmail.com>
2024-03-08 14:08:36 +0900

    싱글톤 컨테이너

--- src/test/java/hello/core/singleton/singletonTest.java
+++ src/test/java/hello/core/singleton/singletonTest.java
@@ -48,5 +48,16 @@ public class singletonTest {
         singletonService1.logic();
     }
 
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(Appconfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
 
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+        // memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
+    }
 }
