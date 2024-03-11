b1269666b12b03ff9452ca2b3cedf03f59a51a62
SUN <siksik2782259@gmail.com>
2024-03-11 12:38:12 +0900

    롬복과 최신 트랜드

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -4,9 +4,11 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import org.springframework.context.annotation.*;
 
+import java.lang.module.Configuration;
+
 import static org.springframework.context.annotation.ComponentScan.*;
 
-@Configuration
+@org.springframework.context.annotation.Configuration
 @ComponentScan(
         // basePackages = "hello.core.member",
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,21 +4,23 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 // 할인 정책 애플리케이션
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
     // 인터페이스에서만 의존하도록 설정
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired --> @RequiredArgsConstructor
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
